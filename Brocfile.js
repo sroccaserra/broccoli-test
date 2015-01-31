0bedc563e8d33c8a178e28b5ed26c0014b80c03a
sroccaserra <sroccaserra@yahoo.com>
2015-01-31 23:06:29 +0100

    Update Brocfile.js

--- Brocfile.js
+++ Brocfile.js
@@ -1,12 +1,11 @@
 var staticCompiler = require('broccoli-static-compiler');
 var fileRemover = require('broccoli-file-remover');
-var sixToFive = require('broccoli-6to5-transpiler');
+var sixToFiveTranspiler = require('broccoli-6to5-transpiler');
 var mergeTrees = require('broccoli-merge-trees');
 
 var app = fileRemover('app', {path: '/vendor'});
+var transpiled = sixToFiveTranspiler(app, {modules: 'amd'});
 
 var libs = staticCompiler('app', {srcDir: '/vendor', destDir: '/vendor'})
-
-var transpiled = sixToFive(app, {modules: 'amd'});
 
 module.exports = mergeTrees([transpiled, libs]);
